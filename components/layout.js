6bc1f15ac595005e21810fafa80cfaceb35e5ab1
Mareike <mareike.brandt@hotmail.de>
2022-10-14 15:14:23 +0900

    feat: added firebase configs

--- components/layout.js
+++ components/layout.js
@@ -2,7 +2,7 @@ import Nav from "./Nav";
 
 export default function Layout({ children }) {
   return (
-    <div className="mx-6 md:mx-w-2xl bg-black">
+    <div className="mx-6 md:max-w-2xl md:mx-auto font-poppins ">
       <Nav />
       <main>{children}</main>
     </div>
