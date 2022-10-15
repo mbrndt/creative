a836db75c1ed81aa3f7373c6f96e675630eeb103
Mareike <mareike.brandt@hotmail.de>
2022-10-15 18:26:53 +0900

    feat: set up login page

--- components/Nav.js
+++ components/Nav.js
@@ -1,10 +1,20 @@
 import React from "react";
+import Link from "next/link";
 
 function Nav() {
   return (
-    <div>
-      <h1>Nav</h1>
-    </div>
+    <nav className="flex justify-center items-center py-10">
+      <Link href="/">
+        <button className="text-lg font-medium">Creative Minds</button>
+      </Link>
+      <ul className="flex items-centeer gap-10">
+        <Link href="/auth/login">
+          <a className="py-2 px-4 text-sm bg-cyan-500 text-white rounded-lg font-medium ml-8">
+            Join Now
+          </a>
+        </Link>
+      </ul>
+    </nav>
   );
 }
 
---
+++ pages/auth/login.js
@@ -0,0 +1,30 @@
+import { FcGoogle } from "react-icons/fc";
+import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { auth } from "../../utils/firebase";
+
+export default function Login() {
+  const googleProvider = new GoogleAuthProvider();
+  const GoogleLogin = async () => {
+    try {
+      const result = await signInWithPopup(auth, googleProvider);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div className="shadow-xl mt-32 p-10 text-gray-700 rounded-lg">
+      <h2 className="text-2xl font-medium">Join Today</h2>
+      <div className="py-4">
+        <h3 className="py-4">Sign in with one of the providers</h3>
+        <button
+          onClick={GoogleLogin}
+          className="text-white bg-gray-600 w-full font-medium rounded-lg flex align-middle p-4 gap-2"
+        >
+          <FcGoogle className="text-2xl" />
+          Sign in with Google
+        </button>
+      </div>
+    </div>
+  );
+}
